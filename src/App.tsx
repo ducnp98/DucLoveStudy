6c46c16bf5976915b2eaeefb3802a5a5ce9861ee
ducnp98 <duc.nguyenphuoc160198@gmail.com>
2024-05-26 15:46:09 +0700

    26/5/2024: Add styled-component, tailwind and initial UI for button

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-white py-8 px-8">
+      <ContainerButton className="w-36 h-14 bg-red-300 flex justify-center items-center rounded-lg">
+        <p className="text-white text-base">Hello</p>
+      </ContainerButton>
     </div>
   );
-}
+};
+
+const ContainerButton = styled.div`
+  box-shadow: -4px -3px 19px 5px #d3d0d0;
+`;
 
 export default App;
